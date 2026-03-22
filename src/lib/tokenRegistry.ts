36e33b41801010adc46581fe98bec0fbbce45687
Gamer <gamertechs.defi@gmail.com>
2026-03-22 21:42:50 +0100

    feat(new listing): added $AIDE (AI DEFI)

--- src/lib/tokenRegistry.ts
+++ src/lib/tokenRegistry.ts
@@ -163,6 +163,7 @@ export const TOKEN_REGISTRY: TokenMetadata[] = [
   { address: "0xE1bf97baCF682AE17cA3E239aB68a7b525f994A4", symbol: "zuba", name: "Zuba TheSuperDog", chain:"bsc", isBurn: false},
   { address: "0x3925f2ae71bCd36b9e4284F92f519f3924b2A91a", symbol: "spt", name: "SAFEPOINT TOKEN", chain: "bsc", isBurn: false},
   { address: "0x5944C5278298ADfa691d29ceA70e29333dD47777", symbol: "gpan", name: "Giant Panda", chain: "bsc", isBurn: false},
+    { address: "0xA925EbCF141c5efdDA5c38c569Ab789aa52f0Fc3", symbol: "aide", name: "AI DEFI", chain: "bsc", isBurn: false},
 
   {address: "0xf20f989CAf263C513f9183B4Fed88F14Fc04c8dB", symbol: "shalom", name: "Shalom", chain: "rwa", isBurn: false },
   { address: "0x782ea82124B474f1f968262ec24FCdED39689dd5", symbol: "rvm", name: "Real World Meme", chain: "rwa", isBurn: false},
