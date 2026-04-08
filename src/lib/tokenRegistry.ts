b225741bb22a24db4097de5a42d27807f71b8d86
Gamer <gamertechs.defi@gmail.com>
2026-04-08 11:26:45 +0100

    feat(new listing): added $BGOAT (BNBGOAT(

--- src/lib/tokenRegistry.ts
+++ src/lib/tokenRegistry.ts
@@ -171,6 +171,7 @@ export const TOKEN_REGISTRY: TokenMetadata[] = [
   { address: "0x47318Ce01d3c447acA06A7bbBd25a35Ad1184D96", symbol: "gcat", name: "Glory Cat", chain: "bsc", isBurn: false },
   { address: "0x6fc20e595A8704725DBd160E7c799665706e0bdD", symbol: "lexa", name: "Lexa Coin", chain: "bsc", isBurn: false},
     { address: "0x4CF42771B73dACb4C2c78790dA77D64a9ae6bdBd", symbol: "kinka", name: "Kinkajou Token", chain: "bsc", isBurn: false},
+    { address: "0x81DD6A9395520263Ded0BdB9578274ab9d3Be186", symbol: "bgoat", name: "BNBGOAT", chain: "bsc", isBurn: false},
 
 
   { address: "0xf20f989CAf263C513f9183B4Fed88F14Fc04c8dB", symbol: "shalom", name: "Shalom", chain: "rwa", isBurn: false },
