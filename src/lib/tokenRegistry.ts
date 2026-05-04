9eee1ed7ad2560c79b50811cdb76774251698f95
Gamer <gamertechs.defi@gmail.com>
2026-05-04 22:29:59 +0100

    fix

--- src/lib/tokenRegistry.ts
+++ src/lib/tokenRegistry.ts
@@ -190,7 +190,7 @@ export const TOKEN_REGISTRY: TokenMetadata[] = [
 
   { address: "ERpXkEafaKuKEARBCFsVnLZA1GARWUjBBbQCukXpbonk", symbol: "REST", name: "REST", chain: "sol", isBurn: false },
   { address: "3roGjzv4TeaWFN8VN6hfUKAsmoVUrYmmB2Dz3NYF1GAA", symbol: "FINU", name: "Fox Inu", chain: "sol", isBurn: false },
-  { address: "NUKEB18Z7R2O9DT15UU5SJPCVSMKCSUAWJN1XCH48JR", symbol: "nuke", name: "Roburt Oppunheimer", chain: "sol", isBurn: false },
+  { address: "NUKEB18Z7r2o9dT15uu5sjpcvsMKCsUAwJN1xch48JR", symbol: "nuke", name: "Roburt Oppunheimer", chain: "sol", isBurn: false },
 
 
 ];
